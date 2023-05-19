c43e318e12b7399175415377e2d5b594e95bbb4f
Alejandro <jaaneo@gmail.com>
2023-05-19 00:59:08 -0400

    update all routes

--- src/app/town/store/page.tsx
+++ src/app/town/store/page.tsx
@@ -1,5 +1,22 @@
+import Button from '@/frontend/components/common/Button'
+import Link from 'next/link'
+
 export default function TownStorePage (): JSX.Element {
   return (
-    <>Town Store</>
+    <div className="min-h-screen flex flex-col items-center justify-center gap-4 px-10">
+      <h1 className="text-2xl">
+        ¡Store Greedy Band!
+      </h1>
+      <p className="text-center">
+        En nuestra tienda, podrás encontrar todo lo necesario para asegurarte la victoria en cada batalla.
+      </p>
+      <Button
+        as={Link}
+        role="button"
+        href="/town"
+      >
+        Volver
+      </Button>
+    </div>
   )
 }
